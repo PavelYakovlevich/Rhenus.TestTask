b2377f8f6f47961fb6dcc04ad9f9855b127e97f5
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2023-08-18 18:33:28 +0300

    Implement users controller.

--- src/Services/Users/Users.API/Controllers/UsersController.cs
+++ src/Services/Users/Users.API/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Models.Users;
+using Users.Contract.Services;
+using Users.Domain.Models;
 
 namespace ManagementApp.API.Controllers;
 
@@ -8,27 +11,53 @@ namespace ManagementApp.API.Controllers;
 [Route("[controller]")]
 public class UsersController : ControllerBase
 {
+    private readonly IUserService _service;
+    private readonly IMapper _mapper;
+    private readonly CancellationToken _tokenMock;
+
+    public UsersController(IUserService service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+
+        using var tokenSource = new CancellationTokenSource();
+        _tokenMock = tokenSource.Token;
+    }
+    
     [HttpPost]
     public async Task<IActionResult> CreateUser(UserModel user)
     {
-        throw new NotImplementedException();
+        var model = _mapper.Map<User>(user);
+
+        var id = await _service.CreateAsync(model, _tokenMock);
+        
+        return CreatedAtAction(nameof(CreateUser), id);
     }
     
     [HttpDelete]
     public async Task<IActionResult> DeleteUser(Guid id)
     {
-        throw new NotImplementedException();
+        await _service.DeleteAsync(id, _tokenMock);
+
+        return NoContent();
     }
     
     [HttpPut]
-    public async Task<IActionResult> UpdateUser([Required] Guid id, UserModel model)
+    public async Task<IActionResult> UpdateUser([Required] Guid id, UserModel user)
     {
-        throw new NotImplementedException();
+        var model = _mapper.Map<User>(user);
+        
+        await _service.UpdateAsync(id, model, _tokenMock);
+
+        return NoContent();
     }
 
     [HttpGet]
-    public IAsyncEnumerable<UserModel> GetUsers([FromQuery] int skip, [FromQuery] int count)
+    public async IAsyncEnumerable<UserModel> GetUsers([FromQuery] int skip, [FromQuery] int count)
     {
-        throw new NotImplementedException();
+        await foreach (var user in _service.ReadAsync(skip, count, _tokenMock))
+        {
+            yield return _mapper.Map<UserModel>(user);
+        }
     }
 }
