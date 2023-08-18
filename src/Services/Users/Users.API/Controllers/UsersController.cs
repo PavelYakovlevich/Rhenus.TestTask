3bb7c6b5ec0dacf5c4bdcd7a1f3259185d1ff555
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2023-08-18 16:21:39 +0300

    Rework project structure.

---
+++ src/Services/Users/Users.API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementApp.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public async Task<Guid> CreateUser()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> DeleteUser(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateUser(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
