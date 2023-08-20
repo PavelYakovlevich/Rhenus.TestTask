34619a6ec45289d92cef605975dfa0ce74601025
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2023-08-20 14:41:19 +0300

    Add account controller.

---
+++ src/Management.API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Users;
+using CreateUserModel = Users.Domain.Models.CreateUserModel;
+
+namespace ManagementApp.API.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AccountController : ControllerBase
+{
+    [HttpPost("registration")]
+    public async Task<IActionResult> Register([FromBody] CreateUserModel user)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+}
