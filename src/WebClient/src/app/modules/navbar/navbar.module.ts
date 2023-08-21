b14384015255c5d2099bcfaab43a7932e90cad2b
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2023-08-21 20:08:20 +0300

    Add layout for navbar.

--- src/WebClient/src/app/modules/navbar/navbar.module.ts
+++ src/WebClient/src/app/modules/navbar/navbar.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
     NavbarComponent
