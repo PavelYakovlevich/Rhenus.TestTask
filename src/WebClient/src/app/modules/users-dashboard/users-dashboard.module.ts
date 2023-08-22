fd15b53e3e36cf913493d226221745afc84bcead
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2023-08-23 00:28:43 +0300

    Add spinner, fix stylings.

--- src/WebClient/src/app/modules/users-dashboard/users-dashboard.module.ts
+++ src/WebClient/src/app/modules/users-dashboard/users-dashboard.module.ts
@@ -8,6 +8,7 @@ import { UsersListComponent } from './components/users-list/users-list.component
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 
@@ -30,7 +31,8 @@ import { ConfirmationDialogModule } from '../shared/confirmation-dialog/confirma
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
-    ConfirmationDialogModule
+    ConfirmationDialogModule,
+    MatProgressBarModule
   ],
   exports: [
     UsersDashboardComponent
